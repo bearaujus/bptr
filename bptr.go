ce50522e2dfe4e15fc6c30f42f1e97062600e200
Haryo Bagas Assyafah <64161364+bearaujus@users.noreply.github.com>
2024-11-18 01:43:00 +0700

    Update code documentation

--- bptr.go
+++ bptr.go
@@ -1,325 +1,253 @@
 package bptr
 
-// ToBoolSafe safely convert *bool to bool.
-//
-// If the input pointer is nil, return the default value.
+// ToBoolSafe converts a *bool to a bool safely.
+// If the input pointer is nil, it returns false.
 func ToBoolSafe(v *bool) bool {
-	// If v is nil ptr bool, return default value
 	if v == nil {
 		return false
 	}
-	// If v is not nil ptr bool, dereference it
 	return *v
 }
 
-// ToByteSafe safely convert *byte to byte.
-//
-// If the input pointer is nil, return the default value.
+// ToByteSafe converts a *byte to a byte safely.
+// If the input pointer is nil, it returns 0.
 func ToByteSafe(v *byte) byte {
-	// If v is nil ptr byte,return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr byte, dereference it
 	return *v
 }
 
-// ToComplex64Safe safely convert *complex64 to complex64.
-//
-// If the input pointer is nil, return the default value.
+// ToComplex64Safe converts a *complex64 to a complex64 safely.
+// If the input pointer is nil, it returns 0.
 func ToComplex64Safe(v *complex64) complex64 {
-	// If v is nil ptr complex64, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr complex64, dereference it
 	return *v
 }
 
-// ToComplex128Safe safely convert *complex128 to complex128.
-//
-// If the input pointer is nil, return the default value.
+// ToComplex128Safe converts a *complex128 to a complex128 safely.
+// If the input pointer is nil, it returns 0.
 func ToComplex128Safe(v *complex128) complex128 {
-	// If v is nil ptr complex128, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr complex128, dereference it
 	return *v
 }
 
-// ToFloat32Safe safely convert *float32 to float32.
-//
-// If the input pointer is nil, return the default value.
+// ToFloat32Safe converts a *float32 to a float32 safely.
+// If the input pointer is nil, it returns 0.
 func ToFloat32Safe(v *float32) float32 {
-	// If v is nil ptr float32, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr float32, dereference it
 	return *v
 }
 
-// ToFloat64Safe safely convert *float64 to float64.
-//
-// If the input pointer is nil, return the default value.
+// ToFloat64Safe converts a *float64 to a float64 safely.
+// If the input pointer is nil, it returns 0.
 func ToFloat64Safe(v *float64) float64 {
-	// If v is nil ptr float64, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr float64, dereference it
 	return *v
 }
 
-// ToIntSafe safely convert *int to int.
-//
-// If the input pointer is nil, return the default value.
+// ToIntSafe converts a *int to an int safely.
+// If the input pointer is nil, it returns 0.
 func ToIntSafe(v *int) int {
-	// If v is nil ptr int, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr int, dereference it
 	return *v
 }
 
-// ToInt8Safe safely convert *int8 to int8.
-//
-// If the input pointer is nil, return the default value.
+// ToInt8Safe converts a *int8 to an int8 safely.
+// If the input pointer is nil, it returns 0.
 func ToInt8Safe(v *int8) int8 {
-	// If v is nil ptr int8, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr int8, dereference it
 	return *v
 }
 
-// ToInt16Safe safely convert *int16 to int16.
-//
-// If the input pointer is nil, return the default value.
+// ToInt16Safe converts a *int16 to an int16 safely.
+// If the input pointer is nil, it returns 0.
 func ToInt16Safe(v *int16) int16 {
-	// If v is nil ptr int16, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr int16, dereference it
 	return *v
 }
 
-// ToInt32Safe safely convert *int32 to int32.
-//
-// If the input pointer is nil, return the default value.
+// ToInt32Safe converts a *int32 to an int32 safely.
+// If the input pointer is nil, it returns 0.
 func ToInt32Safe(v *int32) int32 {
-	// If v is nil ptr int32, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr int32, dereference it
 	return *v
 }
 
-// ToInt64Safe safely convert *int64 to int64.
-//
-// If the input pointer is nil, return the default value.
+// ToInt64Safe converts a *int64 to an int64 safely.
+// If the input pointer is nil, it returns 0.
 func ToInt64Safe(v *int64) int64 {
-	// If v is nil ptr int64, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr int64, dereference it
 	return *v
 }
 
-// ToRuneSafe safely convert *rune to rune.
-//
-// If the input pointer is nil, return the default value.
+// ToRuneSafe converts a *rune to a rune safely.
+// If the input pointer is nil, it returns 0.
 func ToRuneSafe(v *rune) rune {
-	// If v is nil ptr rune, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr rune, dereference it
 	return *v
 }
 
-// ToStringSafe safely convert *string to string.
-//
-// If the input pointer is nil, return the default value.
+// ToStringSafe converts a *string to a string safely.
+// If the input pointer is nil, it returns an empty string.
 func ToStringSafe(v *string) string {
-	// If v is nil ptr string, return default value
 	if v == nil {
 		return ""
 	}
-	// If v is not nil ptr string, dereference it
 	return *v
 }
 
-// ToUintSafe safely convert *uint to uint.
-//
-// If the input pointer is nil, return the default value.
+// ToUintSafe converts a *uint to a uint safely.
+// If the input pointer is nil, it returns 0.
 func ToUintSafe(v *uint) uint {
-	// If v is nil ptr uint, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr uint, dereference it
 	return *v
 }
 
-// ToUint8Safe safely convert *uint8 to uint8.
-//
-// If the input pointer is nil, return the default value.
+// ToUint8Safe converts a *uint8 to a uint8 safely.
+// If the input pointer is nil, it returns 0.
 func ToUint8Safe(v *uint8) uint8 {
-	// If v is nil ptr uint8, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr uint8, dereference it
 	return *v
 }
 
-// ToUint16Safe safely convert *uint16 to uint16.
-//
-// If the input pointer is nil, return the default value.
+// ToUint16Safe converts a *uint16 to a uint16 safely.
+// If the input pointer is nil, it returns 0.
 func ToUint16Safe(v *uint16) uint16 {
-	// If v is nil ptr uint16, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr uint16, dereference it
 	return *v
 }
 
-// ToUint32Safe safely convert *uint32 to uint32.
-//
-// If the input pointer is nil, return the default value.
+// ToUint32Safe converts a *uint32 to a uint32 safely.
+// If the input pointer is nil, it returns 0.
 func ToUint32Safe(v *uint32) uint32 {
-	// If v is nil ptr uint32, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr uint32, dereference it
 	return *v
 }
 
-// ToUint64Safe safely convert *uint64 to uint64.
-//
-// If the input pointer is nil, return the default value.
+// ToUint64Safe converts a *uint64 to a uint64 safely.
+// If the input pointer is nil, it returns 0.
 func ToUint64Safe(v *uint64) uint64 {
-	// If v is nil ptr uint64, return default value
 	if v == nil {
 		return 0
 	}
-	// If v is not nil ptr uint64, dereference it
 	return *v
 }
 
-// FromBool convert *bool to bool.
+// FromBool converts a bool to a *bool.
 func FromBool(v bool) *bool {
-	// Reference the bool object to ptr bool
 	return &v
 }
 
-// FromByte convert *byte to byte.
+// FromByte converts a byte to a *byte.
 func FromByte(v byte) *byte {
-	// Reference the byte object to ptr byte
 	return &v
 }
 
-// FromComplex64 convert *complex64 to complex64.
+// FromComplex64 converts a complex64 to a *complex64.
 func FromComplex64(v complex64) *complex64 {
-	// Reference the complex64 object to ptr complex64
 	return &v
 }
 
-// FromComplex128 convert *complex128 to complex128.
+// FromComplex128 converts a complex128 to a *complex128.
 func FromComplex128(v complex128) *complex128 {
-	// Reference the complex128 object to ptr complex128
 	return &v
 }
 
-// FromFloat32 convert *float32 to float32.
+// FromFloat32 converts a float32 to a *float32.
 func FromFloat32(v float32) *float32 {
-	// Reference the float32 object to ptr float32
 	return &v
 }
 
-// FromFloat64 convert *float64 to float64.
+// FromFloat64 converts a float64 to a *float64.
 func FromFloat64(v float64) *float64 {
-	// Reference the float64 object to ptr float64
 	return &v
 }
 
-// FromInt convert *int to int.
+// FromInt converts an int to a *int.
 func FromInt(v int) *int {
-	// Reference the int object to ptr int
 	return &v
 }
 
-// FromInt8 convert *int8 to int8.
+// FromInt8 converts an int8 to a *int8.
 func FromInt8(v int8) *int8 {
-	// Reference the int8 object to ptr int8
 	return &v
 }
 
-// FromInt16 convert *int16 to int16.
+// FromInt16 converts an int16 to a *int16.
 func FromInt16(v int16) *int16 {
-	// Reference the int16 object to ptr int16
 	return &v
 }
 
-// FromInt32 convert *int32 to int32.
+// FromInt32 converts an int32 to a *int32.
 func FromInt32(v int32) *int32 {
-	// Reference the int32 object to ptr int32
 	return &v
 }
 
-// FromInt64 convert *int64 to int64.
+// FromInt64 converts an int64 to a *int64.
 func FromInt64(v int64) *int64 {
-	// Reference the int64 object to ptr int64
 	return &v
 }
 
-// FromRune convert *rune to rune.
+// FromRune converts a rune to a *rune.
 func FromRune(v rune) *rune {
-	// Reference the rune object to ptr rune
 	return &v
 }
 
-// FromString convert *string to string.
+// FromString converts a string to a *string.
 func FromString(v string) *string {
-	// Reference the string object to ptr string
 	return &v
 }
 
-// FromUint convert *uint to uint.
+// FromUint converts a uint to a *uint.
 func FromUint(v uint) *uint {
-	// Reference the uint object to ptr uint
 	return &v
 }
 
-// FromUint8 convert *uint8 to uint8.
+// FromUint8 converts a uint8 to a *uint8.
 func FromUint8(v uint8) *uint8 {
-	// Reference the uint8 object to ptr uint8
 	return &v
 }
 
-// FromUint16 convert *uint16 to uint16.
+// FromUint16 converts a uint16 to a *uint16.
 func FromUint16(v uint16) *uint16 {
-	// Reference the uint16 object to ptr uint16
 	return &v
 }
 
-// FromUint32 convert *uint32 to uint32.
+// FromUint32 converts a uint32 to a *uint32.
 func FromUint32(v uint32) *uint32 {
-	// Reference the uint32 object to ptr uint32
 	return &v
 }
 
-// FromUint64 convert *uint64 to uint64.
+// FromUint64 converts a uint64 to a *uint64.
 func FromUint64(v uint64) *uint64 {
-	// Reference the uint64 object to ptr uint64
 	return &v
 }
